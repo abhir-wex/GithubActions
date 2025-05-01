3ab59187ae9766612b59e40f203450b3b26419a7
Abhinaya Rangarajan <Abhinaya.Rangarajan@wexinc.com>
2025-05-01 11:18:54 -0400

    Added more tests

--- GradeCheck.nUnitTests/GradeCheckTests.cs
+++ GradeCheck.nUnitTests/GradeCheckTests.cs
@@ -50,5 +50,31 @@ namespace GradeCheck.nUnitTests
             //Assert
             Assert.IsNotNull("B", grade);
         }
+
+        [Test]
+        public void GradePercentage_LessThan60_ReturnsF()
+        {
+            //Assign
+            var percentage = 55;
+
+            //Act
+            var grade = gradeCalculator.GetGrade(percentage);
+
+            //Assert
+            Assert.AreEqual("F", grade);
+        }
+
+        //[Test]
+        //public void GradePercentage_GreaterThan90_ReturnsA()
+        //{
+        //    //Assign
+        //    var percentage = 95;
+
+        //    //Act
+        //    var grade = gradeCalculator.GetGrade(percentage);
+
+        //    //Assert
+        //    Assert.AreEqual("A", grade);
+        //}
     }
 }
